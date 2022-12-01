2c0f3cd9dfb4766a5218867b3b19e7d7d3a9c52d
cosnelle.djoumekoum <casnelle2018@gmail.com>
2022-12-01 15:58:39 +0100

    modification1 index.js et main.py

--- Graphe_chartjs/index.js
+++ Graphe_chartjs/index.js
@@ -1,37 +1,50 @@
+
 const API_URL = "http://127.0.0.1:8000/data_aggregation"
 fetch(API_URL) 
    .then(res => res.json())         //recupère données format json           
-   .then(function(JSONdata) { 
-    const data = {
-      labels: JSONdata.data.Nb_utilisateurs_modeTransport_global.labels,
-      datasets: [{
-        label: "nb_utilisateurs_global",
-        data: JSONdata.data.Nb_utilisateurs_modeTransport_global.data,
-        backgroundColor: ['rgba(54, 162, 235, 0.2)'],
-      },
-      {
-        label: "nb_utilisateurs_univ_lille",
-        data: JSONdata.data.Nb_utilisateurs_modeTransport_univ_Lille.data,
-        backgroundColor: ['rgba(255, 205, 86, 0.2)'],
-      }, 
-      {
-        label: "nb_utilisateurs_univ_Artois",
-        data: JSONdata.data.Nb_utilisateurs_modeTransport_univ_Art.data,
-        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-      },
-      {
-        label: "nb_utilisateurs_univ_valencienne",
-        data: JSONdata.data.Nb_utilisateurs_modeTransport_univ_Val.data,
-        backgroundColor: [ 'rgba(75, 192, 192, 0.2)'],
-      }
-    ]
+   .then( JSONdata => {
+      
+      const labels  = JSONdata.data.Nb_utilisateurs_modeTransport_global.map(
+      function(index){
+        return index.Mode_transport;
+      })
+
+     const data_global = JSONdata.data.Nb_utilisateurs_modeTransport_global.map(
+      function(index){
+        return index.Nb_utilisateurs;
+      }) 
+    
+     console.log(data_global)
+     console.log(labels)
+    
+     const liste_data = Array(JSONdata.data.nb_univ).map(e => Array(JSONdata.data.nb_univ).fill("none"));
+     for (let i = 0; i < JSONdata.data.nb_univ ; i++) {
+      const tab = JSONdata.data.Nb_utilisateurs_modeTransport_univ[i].map((valeur) => valeur.Nb_utilisateurs)
+      liste_data[i] = tab
+     }
+     console.log(liste_data)
+
+    list_color = [ ['rgba(255, 205, 86, 0.2)'],  ['rgba(255, 99, 132, 0.2)'] , [ 'rgba(75, 192, 192, 0.2)'], ['rgba(54, 162, 235, 0.2)'] ]  // jaune, rouge, bleu clair, bleu
+    datasets = []  // def datasets contruire data
+    d_glob = { label: "Nb_utilisateurs_global", data: data_global, backgroundColor: ['rgba(54, 162, 235, 0.2)'] }
+    datasets.push(d_glob)
+    for (let i = 0; i < JSONdata.data.nb_univ ; i++) {
+       const data = { label : JSONdata.data.Nb_utilisateurs_modeTransport_univ[i][0].Université, data: liste_data[i], backgroundColor: list_color[i]  }
+       datasets.push(data)
+    }
+
+    console.log(datasets)
+    
+    const data = {   // def data construire config
+      labels: labels,
+      datasets
     }
     
-    const config = {
+    const config = { // def config pour créer le graphr
       type: 'bar',
       data,
       options : {
-        plugins: {
+        plugins: {        
           title: {
             display: true,
             text: 'Nombre d utilisateur mode de transport par université',
@@ -44,12 +57,19 @@ fetch(API_URL)
       }
     };
   
-    const mygraph = new Chart(
+    const mygraph = new Chart(  // creation du graphe
       document.getElementById('mygraph'), 
       config
     );
+
+    async function getStockage_data() {
+      const API_URL = "http://127.0.0.1:8000/data_aggregation"
+      const res = await fetch(API_URL)
+    }
+
   })
+ 
 
 
-
+ 
    
--- Graphe_chartjs/main.py
+++ Graphe_chartjs/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_origins=["*"] , #permet d'éviter les erreurs d'accès au serveur API lors du fetch
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"] 
 )
 
 @app.get("/")  #endpoint
@@ -26,40 +26,52 @@ async def getData_mobilite():
     file = open('data_mobilite.json')   #Avoir un fichier data_mobilite_json dans le même répertoire
     return json.load(file)
 
+def calcul_nbutilis_modeTransport_univ(frame, Nom_colonne: str):
+    glob = frame[Nom_colonne].value_counts()
+    glob_label = glob.index.sort_values().tolist() 
+    glob_data = glob.values.tolist()
+    
+    res = pd.DataFrame(frame.groupby(["Etablissement"], as_index = False)["Mode"].value_counts()) #Dataframe
+    res = res.groupby("Etablissement") #transformer le dataframe en un DataframeGroupby(liste) objet afin de pouvoir récupérer les différents groupes
+        
 
+    liste_universite, liste_Mode_transport, liste_data = [], [], []
+    for i in range(len(list(res.groups))):
+           universite = list(res)[i][0]
+           liste_transport_univ= list(res)[i][1].sort_values(["Mode"])["Mode"].sort_values().tolist()
+           liste_data_univ = list(res)[i][1].sort_values(["Mode"])["count"].tolist()
 
-Etab = df["Etablissement"].unique()
-df_univ_Lille = df [ df["Etablissement"]  == Etab[0] ]
-df_univ_Art = df [ df["Etablissement"]  == Etab[1] ]
-df_univ_Val= df [ df["Etablissement"]  == Etab[2] ]
+           liste_universite.append(universite )
+           liste_Mode_transport.append(liste_transport_univ)
+           liste_data.append(liste_data_univ)
 
+  
+    liste_data_global = []
+    for i in range(len(glob_label)):     
+       glob_mode =  {"Mode_transport" : glob_label[i], "Nb_utilisateurs" : glob_data[i]}
+       liste_data_global.append(glob_mode)
 
-async def data_analysis(frame, Nom_colonne: str):   #Calcul le nombre d'utilisateur pour chaque moyen de transport
-    dataf = frame[Nom_colonne].value_counts()
-    labels_names = dataf.index.sort_values().tolist()
-    data = dataf.values.tolist()
-    retour_data = {"labels": labels_names, "data": data}
-    return retour_data
-
-
+    
+    univ, data_univ =  [None]*len(liste_universite), []
+    for i in range(len(liste_universite)): 
+          for j in range (len(liste_data[i])):    
+             x =  {"Université": liste_universite[i],"Mode_transport" :liste_Mode_transport[i][j], "Nb_utilisateurs" : liste_data[i][j]}
+             data_univ.append(x)
+          univ[i] = data_univ
+          data_univ = []  
+    
+    data = {"data" : { "nb_univ" : len(liste_universite), "Nb_utilisateurs_modeTransport_global" : liste_data_global,  "Nb_utilisateurs_modeTransport_univ": univ }} 
+    return data
+    
+    
 @app.get("/data_aggregation")  
 async def get_data_aggregation():
-    Nb_utilisateurs_modeTransport_global= await data_analysis(df, "Mode")
-    Nb_utilisateurs_modeTransport_univ_Lille = await data_analysis(df_univ_Lille, "Mode")
-    Nb_utilisateurs_modeTransport_univ_Art = await data_analysis(df_univ_Art, "Mode", )
-    Nb_utilisateurs_modeTransport_univ_Val = await data_analysis(df_univ_Val, "Mode")
+    file = calcul_nbutilis_modeTransport_univ(df, "Mode")
+    return file
 
 
 
-    return {
-        "data" :{ 
-            "Nb_utilisateurs_modeTransport_global": Nb_utilisateurs_modeTransport_global,
-            "Nb_utilisateurs_modeTransport_univ_Lille": Nb_utilisateurs_modeTransport_univ_Lille,
-            "Nb_utilisateurs_modeTransport_univ_Art":  Nb_utilisateurs_modeTransport_univ_Art,
-            "Nb_utilisateurs_modeTransport_univ_Val":  Nb_utilisateurs_modeTransport_univ_Val
-            }
-    
-        }
+  
     
 
 
