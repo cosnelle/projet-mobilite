666748a19d5c2df53a06c25ada11b40a78f98e02
juridolfi <julie.ridolfi@laposte.net>
2022-12-15 08:25:20 +0100

    maj

--- Tableau/tableau.js
+++ Tableau/tableau.js
@@ -1,6 +1,20 @@
-//fetch
+// tableau.js
+// tableau des GES en fonction des modes de transport et des distances
 
-const flux_url = "http://127.0.0.1:8000/geo/flux/person";
+// @elements
+//        DISTANCES
+//        utilisation d'une discretisation des distances
+//        [0;3], [3;5], [5;10], [10;20], [20;50], [50;100], [100;250]
+//        
+//        RECUPERATION DES DONNEES
+//        appel des données se fait avec une API REST fastAPI en python
+//        API lancée en locale
+//        
+//        CALCUL DES GES
+//        pour les calculs nous utiliserons des fonction python directement implémentées dans
+//        l API locale
+
+const url = "http://127.0.0.1:8000/data_mobilite/tableau";
 
 function hideloader(){
   document.getElementById('loading').style.display = 'none';
@@ -8,28 +22,43 @@ function hideloader(){
 
 
 //fonction affichage
+// ici la fonction affichageData est specifique au service créé
 function affichageData(data){
+  // première ligne du tabeau correspond aux distances discrétisées
+  // QUESTION : possibilité d'automatiser cette phase ? Utilité d'automatiser ?
+  //          Si les tranches de distance changent : OUI Sinon ?
   let tab =
       `<tr>
-        <th>Domicile</th>
-        <th>Travail</th>
-        <th>Nbr_personnes</th>
+        <th>  </th>
+        <th> 0 à 3 km </th>
+        <th> 3 à 5 km</th>
+        <th> 5 à 10 km</th>
+        <th> 10 à 20 km</th>
+        <th> 20 à 50 km</th>
+        <th> 50 à 100 km</th>
+        <th> 100 à 250 km</th>
       <tr>`;
-      
+  let i=0; // indice qui nous permet de dire sur quel mode de transport nous sommes
   for (let r of data.data){
-    tab += `<tr>
-        <td>${r.domicile}</td>
-        <td>${r.travail}</td>
-        <td>${r.Nb_person}</td>
+      tab += `<tr>
+        <td>${data.mode[i]}</td>
+        <td>${r.d_00_03}</td>
+        <td>${r.d_03_05}</td>
+        <td>${r.d_05_10}</td>
+        <td>${r.d_10_20}</td>
+        <td>${r.d_20_50}</td>
+        <td>${r.d_50_100}</td>
+        <td>${r.d_100_250}</td>
       <tr>`;
+      i= i+1;
+    
   }
-  
   document.getElementById("flux_pers").innerHTML = tab;
   
 }
   
 //getData
-async function getFlux(url){
+async function getData(url){
   // recuperation de la reponse
   const reponse = await fetch(url);
   
@@ -40,11 +69,6 @@ async function getFlux(url){
     hideloader();
   }
   affichageData(data);
-  
-  return data
-  
 }
 // appel de la fonction getData
-var donnees_flux = getFlux(flux_url);
-
-            
+getData(url);
