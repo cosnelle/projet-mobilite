1d74835244fef00a57740d17a45abe5a7fb8163b
cosnelle.djoumekoum <casnelle2018@gmail.com>
2023-01-13 08:17:52 +0100

    Crétion fichiers fonctions_main contenant les differentes fonctions utilisées dans le main + vérification code PEP8

---
+++ Graphe_chartjs/main.py
@@ -0,0 +1,55 @@
+# main.py
+from fastapi import  FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from fonction_main import *
+
+import pandas as pd 
+
+
+df = pd.read_json("fake-data-v2.json", orient='table')
+
+app = FastAPI()  #créer un objet FastAPI
+
+
+app.add_middleware(
+    CORSMiddleware,   # Cors permet d'autoriser l'accès à des ressources d'origine différente ( ex requêtes fetch sur serveur API)
+    allow_origins=["*"] , #permet d'éviter les erreurs d'accès au serveur API lors du fetch
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")  #endpoint
+async def home():
+    return {"message": "Projet mobilité"}
+
+@app.get("/data_mobilite")
+async def getData_mobilite():
+    file = open('fake-data-v2.json')   #Avoir un fichier data_mobilite_json dans le même répertoire
+    return json.load(file)
+
+
+@app.get("/Nombre_mode_transport_univ")  
+async def get_Nombre_mode_transport_univ():
+    file = Nombre_mode_transport_univ(df)
+    return file
+
+
+@app.get("/Distance_mode_transport_univ_km")  
+async def get_mode_transport_univ():
+    file = Distance_mode_transport_univ(df)
+    return file
+
+@app.get("/Emission_Co2_mode_transport_univ_kgCo2")  
+async def get_Emission_Co2_mode_transport_univ():
+    file = Emission_Co2_mode_transport_univ(df)
+    return file
+
+
+    
+
+
+
+
+
